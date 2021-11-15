a79981759b9f892981c89c8c659149e0c631b842
remained3 <sarahthebush@hotmail.com>
2021-11-15 19:10:49 +0000

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+puts@store1.name
+@store2= Store.find(2)
+puts@store2.name
+@store1.name = "Changed store"
+puts @store1.name
+
+
+#Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
+# Load the second store from the database and assign it to `@store2`.
+# Update the first store (`@store1`) instance in the database. (Change its name or something.)
